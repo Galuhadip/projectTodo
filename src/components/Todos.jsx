412c6a5b885d9a8940ac15ca725f152dda274a63
galuhadipr@gmail.com <galuhadipr@gmail.com>
2024-07-19 16:33:56 +0700

    add components

---
+++ src/components/Todos.jsx
@@ -0,0 +1,21 @@
+import TodoItem from './TodoItem'
+
+const Todos = ({ todos }) => {
+    return (
+      <div style={styles.container}>
+        
+        {todos.map((todo) => {
+        return <TodoItem key={todo.id} todo={todo} />
+      })}
+      </div>
+    )
+  }
+
+  const styles = {
+    container: {
+      width: 'auto',
+      margin: '0 auto'
+    },
+  }
+  
+  export default Todos
